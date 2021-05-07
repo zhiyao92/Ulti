6b098ce89127906f82be8d885f7e0bb1d84388d0
Tan, Zhi Yao [Kelvin] <zhi-yao_tan@astro.com.my>
2021-05-07 17:39:00 +0800

    Added Example

--- Example/Ulti/ViewController.swift
+++ Example/Ulti/ViewController.swift
@@ -7,18 +7,17 @@
 //
 
 import UIKit
+import Ulti
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var mainView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mainView.backgroundColor = .green
+        mainView.addShadow()
+        mainView.addCornerRadius()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
-
